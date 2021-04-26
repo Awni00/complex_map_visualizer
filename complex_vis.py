75714045c4c8b653f347d2afb78f6927673765b5
Awni00 <awnyaltabaa@gmail.com>
2021-04-26 16:37:19 -0400

    added domain-colored plotting functionality

--- complex_vis.py
+++ complex_vis.py
@@ -2,6 +2,7 @@ from sympy import *
 import numpy as np
 
 import matplotlib.pyplot as plt
+import matplotlib.colors
 
 # experiment w/ styles or create one
 plt.style.use('seaborn')
@@ -161,3 +162,95 @@ def define_mobius_transform(matrix):
     (a, b), (c, d) = matrix
     mobius_tfrm = lambda z: (a*z + b) / (c*z + d)
     return mobius_tfrm
+
+# DOMAIN COLOURING
+
+def mag_shading(z_mag):
+    '''perform magnitude shading'''
+    return 0.5 + 0.5 * (z_mag - np.floor(z_mag))
+
+def grid_lines(z, thresh=0.1):
+    '''
+    generate gride line shading from complex number outputs.
+
+    Outlines near-integer values of real and imaginary parts.
+    '''
+
+    return np.abs(np.sin(np.pi * np.real(z))) ** thresh \
+            * np.abs(np.sin(np.pi * np.imag(z))) ** thresh
+
+
+def domain_color_plot(f, real_lims=(-1,1), imag_lims=(-1,1), base_sat_val=None,
+                        mag_as='saturation', gid_lines=True, mag_shading=mag_shading,
+                        figsize=(8,8), resolution=(1000,1000), title=None):
+    """plots domain colored output space of given function
+
+    Args:
+        f (callable): function of complex number. needs to be vectorized (numpy-like)
+        real_lims (float tuple, optional): limits of real axis. Defaults to (-1,1).
+        imag_lims (float tuple, optional): limits of imaginary axis. Defaults to (-1,1).
+        base_sat_val (float, optional): the base value of the remaining colour channel. Defaults to None.
+        mag_as (str, optional): the color channel corresponding to complex magnitude. 
+        one of 'saturation' or 'value'. Defaults to 'saturation'.
+        gid_lines (bool, optional): whether to highlight grid lines. Defaults to True.
+        mag_shading (callable, optional): magnitude shading function. Defaults to mag_shading.
+        figsize (int tuple, optional): size of figure in inches. Defaults to (8,8).
+        resolution (int tuple, optional): resolution of graph per axis. Defaults to (1000,1000).
+        title (str, optional): title of graph. Defaults to None.
+
+    Returns:
+        (fig, ax) tuple of matplotlib figure and axis
+    """
+
+    if base_sat_val == None:
+        if mag_as == 'saturation':
+            base_sat_val = 1
+        elif mag_as == 'value':
+            base_sat_val = 0.75
+
+    if mag_as not in ['saturation', 'value']:
+        raise ValueError("`mag_as` needs to be one of 'saturation' or 'value'")
+
+    # create input space
+    real_space = np.linspace(*real_lims, num=resolution[0])
+    imag_space = np.linspace(*imag_lims, num=resolution[1])
+    xv, yv = np.meshgrid(real_space, imag_space)
+    z_in = xv + yv*1j
+
+    # map to output space
+    z_out = f(z_in)
+
+    # color output space
+    z_phase = np.angle(z_out, deg=True) # phase
+    z_phase = z_phase * (z_phase >= 0) + (360 + z_phase) * (z_phase < 0) # map from (-180, 180] to [0, 360)
+    z_phase = z_phase/360 # normalized phase as hue
+
+    z_mag = np.abs(z_out) # magnitude
+
+
+    if mag_shading:
+        z_mag = mag_shading(z_mag) # perform magnirude shading
+    else:
+        z_mag = z_mag / np.max(z_mag) # normalized magnitude as value
+
+    if grid_lines:
+        value = grid_lines(z_out)
+    else:
+        value = np.ones_like(z_phase)*base_sat_val
+
+    if mag_as == 'saturation':
+        hsv_color = np.stack([z_phase, z_mag, value], axis=-1) # create hsv image
+    elif mag_as == 'value':
+        hsv_color = np.stack([z_phase, value, z_mag], axis=-1) # create hsv image
+
+
+    rgb_color = matplotlib.colors.hsv_to_rgb(hsv_color)
+
+    fig, ax = plt.subplots(figsize=figsize)
+    ax.grid(False)
+    ax.imshow(rgb_color, aspect='equal', extent=(*real_lims, *imag_lims))
+    ax.set_xlabel('Re')
+    ax.set_ylabel('Im')
+    if title: ax.set_title(title)
+
+    return fig, ax
